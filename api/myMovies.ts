d6984f50391943acd50e0bcf2f2f2d8ae8ea22c3
Artyom Galay <artemijgalaj@gmail.com>
2023-05-17 23:18:06 +0600

    Changed api path

--- api/myMovies.ts
+++ api/myMovies.ts
@@ -4,13 +4,13 @@ import { MyMovieItem } from "@/interfaces/my-movie.interface";
 
 export const getAll = async (): Promise<MyMovieItem[]> => {
 	const response = await axios.get(
-		"http://localhost:3000/api/myMovies"
+		"https://softlix-gules.vercel.app/api/myMovies"
 	);
 	return response.data.data;
 };
 export const getMovieById = async (id: number): Promise<MyMovieItem> => {
 	const response = await axios.get(
-		`http://localhost:3000/api/myMovies?id=${id}`
+		`https://softlix-gules.vercel.app/api/myMovies?id=${id}`
 	);
 	return response.data.data;
 };
